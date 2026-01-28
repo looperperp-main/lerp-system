e97786594223058f42143fe27d47d1fb818f78a1
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-01-28 10:51:25 -0300

    Criação dos arquivos do Front-end

---
+++ Angular/erp-front-end-web/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Importante: aponta para seus arquivos Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
