8ae1ae331bfd2bb5bb570d959807b9526b6dd50a
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-01-28 23:13:31 -0300

    Ajustes no liquibase

--- liquibase-service/src/main/java/com/l/erp/liquibase/LiquibaseApplication.java
+++ liquibase-service/src/main/java/com/l/erp/liquibase/LiquibaseApplication.java
@@ -1,13 +1,37 @@
 package com.l.erp.liquibase;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class MigrationApplication {
+public class LiquibaseApplication {
 
     public static void main(String[] args) {
         // Esta aplicação sobe, roda o Liquibase e desliga automaticamente
-        SpringApplication.run(MigrationApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(LiquibaseApplication.class, args);
+
+        // Teste simples para ver se a propriedade foi carregada
+        String url = context.getEnvironment().getProperty("spring.datasource.url");
+        System.out.println("DEBUG: URL do Banco carregada: " + url);
+
+        String liquibaseEnabled = context.getEnvironment().getProperty("spring.liquibase.enabled");
+        System.out.println("DEBUG: Liquibase Enabled? " + liquibaseEnabled);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+
+        // Configurações do seu application.properties que precisamos passar manualmente aqui
+        liquibase.setDropFirst(true);
+        liquibase.setShouldRun(true);
+        return liquibase;
     }
 }
