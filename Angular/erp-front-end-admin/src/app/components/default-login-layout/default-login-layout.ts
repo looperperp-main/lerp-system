377631189d5927c9efc5bc2677a9b24c6d9dfa15
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-01-30 10:49:28 -0300

    feat: Criando a estrutura inicial - parte 1 (Admin)

---
+++ Angular/erp-front-end-admin/src/app/components/default-login-layout/default-login-layout.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [
+    NgOptimizedImage
+
+  ],
+  templateUrl: './default-login-layout.html',
+  styleUrl: './default-login-layout.scss',
+})
+export class DefaultLoginLayout {
+
+}
