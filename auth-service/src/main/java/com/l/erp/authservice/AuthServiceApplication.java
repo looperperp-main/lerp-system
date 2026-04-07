4e5ac061dd4be6855abbf1315bd41267b91a9e16
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-04-06 21:47:43 -0300

    feat: Implementação do Eureka Registry

--- auth-service/src/main/java/com/l/erp/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/l/erp/authservice/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package com.l.erp.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = "com.l.erp")
 @EnableMethodSecurity
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- cadastro-service/src/main/java/com/l/erp/cadastroservice/CadastroServiceApplication.java
+++ cadastro-service/src/main/java/com/l/erp/cadastroservice/CadastroServiceApplication.java
@@ -2,8 +2,10 @@ package com.l.erp.cadastroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CadastroServiceApplication {
 
     public static void main(String[] args) {
