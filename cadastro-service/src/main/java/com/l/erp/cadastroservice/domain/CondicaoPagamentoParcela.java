e26f99ad39db29a5439af636cffde37428f69b35
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-04-13 16:32:19 -0300

    feat: FIX Liquibase

---
+++ cadastro-service/src/main/java/com/l/erp/cadastroservice/domain/CondicaoPagamentoParcela.java
@@ -0,0 +1,66 @@
+package com.l.erp.cadastroservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Table(name = "condicao_pagamento_parcela", schema = "cadastros")
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CondicaoPagamentoParcela {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "condicao_pagamento_id", nullable = false)
+    private CondicaoPagamento condicaoPagamento;
+
+    @NotNull
+    @Column(name = "numero_parcela", nullable = false)
+    private Integer numeroParcela;
+
+    @NotNull
+    @Column(name = "dias", nullable = false)
+    private Integer diasPrazo;
+
+    @NotNull
+    @Column(name = "percentual", nullable = false, precision = 5, scale = 2)
+    private BigDecimal percentual;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @NotNull
+    @Column(name = "created_by", nullable = false)
+    private UUID createdBy;
+
+    @Column(name = "last_updated_by")
+    private UUID lastUpdatedBy;
+}
