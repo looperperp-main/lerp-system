f9717e49c931c77f4b6295cf82fa0daaff00ab11
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-06-02 16:08:11 -0300

    - auth-service, cadastro-service, partner-service, billing-service: remover DockerAvailableCondition.java e @ExtendWith do AbstractIntegrationTest

--- cadastro-service/src/test/java/com/l/erp/cadastroservice/integration/AbstractIntegrationTest.java
+++ cadastro-service/src/test/java/com/l/erp/cadastroservice/integration/AbstractIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.l.erp.cadastroservice.integration;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -11,7 +10,6 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@ExtendWith(DockerAvailableCondition.class)
 @SpringBootTest
 @ActiveProfiles("test")
 @Testcontainers
