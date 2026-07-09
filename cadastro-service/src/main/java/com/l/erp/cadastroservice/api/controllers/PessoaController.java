a90187749862832080db004bd183ab5b4dcc9e80
Vitor Fernandes Ferreira <vitorff1234@gmail.com>
2026-07-09 19:53:59 -0300

    fix: delete PessoaController

--- cadastro-service/src/main/java/com/l/erp/cadastroservice/api/controllers/PessoaController.java
+++ cadastro-service/src/main/java/com/l/erp/cadastroservice/api/controllers/PessoaController.java
@@ -17,6 +17,7 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -133,7 +134,7 @@ public class PessoaController {
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         logger.info("Deletando a Pessoa ID: {}", id);
         Long tenantId = SecurityUtils.getCurrentTenantId().orElseThrow(() -> new BusinessException(Constants.TENANT_NOT_FOUND, HttpStatus.UNAUTHORIZED));
